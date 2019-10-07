a846c9cfe52b1383331c15813afafc3035eb876b
Oleksii Borysonok <alex.borysonok@gmail.com>
2019-10-07 01:18:54 -0400

    now the auto play works along with a step function

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
+/* eslint-disable complexity */
 var gameOfLife = {
-  width: 12,
-  height: 12, // width and height dimensions of the board
+  width: 20,
+  height: 20, // width and height dimensions of the board
   stepInterval: null, // should be used to hold reference to an interval that is "playing" the game
+  virtualTable: {}, // keeps the virtual state for the next step
 
   createAndShowBoard: function() {
     // create <table> element
@@ -23,7 +25,7 @@ var gameOfLife = {
     board.appendChild(goltable);
 
     // once html elements are added to the page, attach events to them
-    this.setupBoardEvents();
+    gameOfLife.setupBoardEvents();
   },
 
   forEachCell: function(iteratorFunc) {
@@ -36,12 +38,14 @@ var gameOfLife = {
     for (let y = 0; y < this.height; y++) {
       for (let x = 0; x < this.width; x++) {
         let cell = document.getElementById(`${x}-${y}`);
-        iteratorFunc(cell);
+        iteratorFunc(cell, x, y);
       }
     }
   },
 
-  onClearClick: function(e) {
+  onClearClick: function() {
+    clearInterval(gameOfLife.stepInterval);
+    gameOfLife.virtualTable = {};
     var clearCell = function(cell) {
       cell.className = 'dead';
       cell.dataset.status = 'dead';
@@ -49,7 +53,7 @@ var gameOfLife = {
     gameOfLife.forEachCell(clearCell);
   },
 
-  onRandomResetClick: function(e) {
+  onRandomResetClick: function() {
     var randomSetCellStatus = function(cell) {
       var rnd = Math.random() * 100;
       if (rnd < 50) {
@@ -74,8 +78,8 @@ var gameOfLife = {
     // clicking on a cell should toggle the cell between "alive" & "dead"
     // for ex: an "alive" cell be colored "blue", a dead cell could stay white
 
-    var onCellClick = function(e) {
-      if (this.dataset.status == 'dead') {
+    var onCellClick = function() {
+      if (this.dataset.status === 'dead') {
         this.className = 'alive';
         this.dataset.status = 'alive';
       } else {
@@ -88,13 +92,19 @@ var gameOfLife = {
       cell.addEventListener('click', onCellClick);
     };
 
-    this.forEachCell(assignClickEvent);
+    gameOfLife.forEachCell(assignClickEvent);
 
     var clearBtn = document.getElementById('clear_btn');
     clearBtn.addEventListener('click', this.onClearClick);
 
     var rndResetBtn = document.getElementById('reset_btn');
     rndResetBtn.addEventListener('click', this.onRandomResetClick);
+
+    var stepBtn = document.getElementById('step_btn');
+    stepBtn.addEventListener('click', this.step);
+
+    var playBtn = document.getElementById('play_btn');
+    playBtn.addEventListener('click', this.enableAutoPlay);
   },
 
   step: function() {
@@ -106,11 +116,68 @@ var gameOfLife = {
     // You need to:
     // 1. Count alive neighbors for all cells
     // 2. Set the next state of all cells based on their alive neighbors
+
+    var countAliveNeighbours = function(x, y) {
+      let aliveNeighbours = 0;
+      for (let h = y - 1; h <= y + 1; h++) {
+        for (let w = x - 1; w <= x + 1; w++) {
+          if (h === y && w === x) {
+            continue;
+          }
+          let cell = document.getElementById(`${w}-${h}`);
+          if (
+            cell &&
+            (cell.className === 'alive' && cell.dataset.status === 'alive')
+          ) {
+            aliveNeighbours++;
+          }
+        }
+      }
+      return aliveNeighbours;
+    };
+
+    var defineVirtualBoardState = function(cell, x, y, state) {
+      gameOfLife.virtualTable[`${x}-${y}`] = state;
+    };
+
+    var setCellNextState = function(cell, x, y) {
+      cell.className = gameOfLife.virtualTable[`${x}-${y}`];
+      cell.dataset.status = gameOfLife.virtualTable[`${x}-${y}`];
+    };
+
+    var defineVirtualNextCellState = function(cell, x, y) {
+      let cellAliveNeighbours = countAliveNeighbours(x, y);
+      if (cell.className === 'alive') {
+        if (cellAliveNeighbours < 2 || cellAliveNeighbours > 3) {
+          defineVirtualBoardState(cell, x, y, 'dead');
+        }
+        if (cellAliveNeighbours === 2 || cellAliveNeighbours === 3) {
+          defineVirtualBoardState(cell, x, y, 'alive');
+        }
+      } else if (cellAliveNeighbours === 3) {
+        defineVirtualBoardState(cell, x, y, 'alive');
+      }
+    };
+
+    // Object.keys(gameOfLife.virtualTable).forEach((key) => {
+    //   gameOfLife.virtualTable[key] = '';
+    // });
+
+    //gameOfLife.virtualTable = {};
+
+    gameOfLife.forEachCell(defineVirtualNextCellState);
+    //gameOfLife.onClearClick();
+    //console.log("===== Next: ", gameOfLife.virtualTable);
+    gameOfLife.forEachCell(setCellNextState);
+    gameOfLife.virtualTable = {};
   },
 
   enableAutoPlay: function() {
     // Start Auto-Play by running the 'step' function
     // automatically repeatedly every fixed time interval
+    gameOfLife.stepInterval = setInterval(() => {
+      gameOfLife.step();
+    }, 500);
   },
 };
 
